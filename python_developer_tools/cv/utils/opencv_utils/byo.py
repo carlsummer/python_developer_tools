6eafd9e351d0c6fb5c995ef19baf9e0d22ec626f
zengxh <zengxh@chint.com>
2021-06-03 11:27:36 +0800

    打印所有的特征图片

--- python_developer_tools/cv/utils/opencv_utils/byo.py
+++ python_developer_tools/cv/utils/opencv_utils/byo.py
@@ -5,6 +5,7 @@
 # @File:byo
 import os
 
+import copy
 import cv2
 import numpy as np
 from ..opencv_utils.new_find import graytoprofile
@@ -36,12 +37,14 @@ def get_adaptive_threshold(dilation_subImg):
     # 自适应二值化
     return cv2.threshold(dilation_subImg, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
+
 def get_threshold(image):
     threshold_binary = 90
     retval, threshold_img = cv2.threshold(image, threshold_binary, 255, cv2.THRESH_BINARY)
     return threshold_img
 
-def get_scharr(o,ddepth=cv2.CV_64F):
+
+def get_scharr(o, ddepth=cv2.CV_64F):
     """获取scharr的特征"""
     scharrx = cv2.Scharr(o, ddepth, 1, 0)
     scharry = cv2.Scharr(o, ddepth, 0, 1)
@@ -52,7 +55,7 @@ def get_scharr(o,ddepth=cv2.CV_64F):
     return scharrx, scharry, scharrxy
 
 
-def get_sobel(img,ddepth=cv2.CV_64F):
+def get_sobel(img, ddepth=cv2.CV_64F):
     x = cv2.Sobel(img, ddepth, 1, 0)
     y = cv2.Sobel(img, ddepth, 0, 1)
 
@@ -62,9 +65,11 @@ def get_sobel(img,ddepth=cv2.CV_64F):
     dst = cv2.addWeighted(absX, 0.5, absY, 0.5, 0)
     return absX, absY, dst
 
+
 def get_laplace(gray):
     laplace = cv2.Laplacian(gray, cv2.CV_8U, ksize=3)
     return laplace
+
 
 def get_blur(img):
     # 均值滤波
@@ -85,11 +90,13 @@ def get_Canny(img):
     # 高斯边缘检测
     return cv2.Canny(img, 0, 50)
 
+
 def get_erode(img):
     # 腐蚀
     kernel = np.ones((5, 5), np.uint8)
     erosion = cv2.erode(img, kernel, iterations=1)
     return erosion
+
 
 def get_dilate(img):
     # 膨胀
@@ -97,11 +104,13 @@ def get_dilate(img):
     dilation = cv2.dilate(img, kernel, iterations=1)
     return dilation
 
+
 def get_open_morph(img):
     # 开运算
     kernel = np.ones((5, 5), np.uint8)
     opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
     return opening
+
 
 def get_close_morph(img):
     # 闭运算 先膨胀后腐蚀
@@ -109,17 +118,20 @@ def get_close_morph(img):
     closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
     return closing
 
+
 def get_gradient(img):
     # 形态学梯度 图像的膨胀和腐蚀之间的差异，结果看起来像目标的轮廓
     kernel = np.ones((5, 5), np.uint8)
     gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
     return gradient
 
+
 def get_tophat(img):
     # 顶帽 原图像与开运算图的区别，突出原图像中比周围亮的区域
     kernel = np.ones((5, 5), np.uint8)
     tophat = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, kernel)
     return tophat
+
 
 def get_blackhat(img):
     # 黑帽 闭运算图 - 原图像,突出原图像中比周围暗的区域
@@ -158,6 +170,7 @@ array([[0, 0, 1, 0, 0],
        [0, 0, 1, 0, 0]], dtype=uint8)
 """
 
+
 def print_all_feature(save_dir, image):
     """打印所有的opencv特征图片
         Args:image是灰度图
@@ -167,69 +180,61 @@ def print_all_feature(save_dir, image):
     resetDir(save_dir)
     cv2.imwrite(os.path.join(save_dir, "origin.jpg"), image)
 
-    adaptive_threshold = get_adaptive_threshold(image)
-    cv2.imwrite(os.path.join(save_dir, "adaptive_threshold.jpg"), adaptive_threshold)
-
-    scharrx64, scharry64, scharrxy64 = get_scharr(image)
-    cv2.imwrite(os.path.join(save_dir, "scharrx64.jpg"), scharrx64)
-    cv2.imwrite(os.path.join(save_dir, "scharrx64_threshold.jpg"), get_threshold(scharrx64))
-    cv2.imwrite(os.path.join(save_dir, "scharry64.jpg"), scharry64)
-    cv2.imwrite(os.path.join(save_dir, "scharry64_threshold.jpg"), get_threshold(scharry64))
-    cv2.imwrite(os.path.join(save_dir, "scharrxy64.jpg"), scharrxy64)
-    cv2.imwrite(os.path.join(save_dir, "scharrxy64_threshold.jpg"), get_threshold(scharrxy64))
-
-    scharrx16, scharry16, scharrxy16 = get_scharr(image,cv2.CV_16S)
-    cv2.imwrite(os.path.join(save_dir, "scharrx16.jpg"), scharrx16)
-    cv2.imwrite(os.path.join(save_dir, "scharrx16_threshold.jpg"), get_threshold(scharrx16))
-    cv2.imwrite(os.path.join(save_dir, "scharry16.jpg"), scharry16)
-    cv2.imwrite(os.path.join(save_dir, "scharry16_threshold.jpg"), get_threshold(scharry16))
-    cv2.imwrite(os.path.join(save_dir, "scharrxy16.jpg"), scharrxy16)
-    cv2.imwrite(os.path.join(save_dir, "scharrxy16_threshold.jpg"), get_threshold(scharrxy16))
-
-    sobelx, sobely, sobelxy = get_sobel(image)
-    cv2.imwrite(os.path.join(save_dir, "sobelx.jpg"), sobelx)
-    cv2.imwrite(os.path.join(save_dir, "sobely.jpg"), sobely)
-    cv2.imwrite(os.path.join(save_dir, "sobelxy.jpg"), sobelxy)
-
-    laplace = get_laplace(image)
-    cv2.imwrite(os.path.join(save_dir, "laplace.jpg"), laplace)
-
-    blur = get_blur(image)
-    cv2.imwrite(os.path.join(save_dir, "blur.jpg"), blur)
-
-    medianBlur = get_medianBlur(image)
-    cv2.imwrite(os.path.join(save_dir, "medianBlur.jpg"), medianBlur)
-
-    GaussianBlur = get_GaussianBlur(image)
-    cv2.imwrite(os.path.join(save_dir, "GaussianBlur.jpg"), GaussianBlur)
-
-    Canny = get_Canny(GaussianBlur)
-    cv2.imwrite(os.path.join(save_dir, "Canny.jpg"), Canny)
-
-    profile = graytoprofile(image)
-    cv2.imwrite(os.path.join(save_dir, "profile.jpg"), profile)
-
-    erode = get_erode(image)
-    cv2.imwrite(os.path.join(save_dir, "erode.jpg"), erode)
-
-    dilate = get_dilate(image)
-    cv2.imwrite(os.path.join(save_dir, "dilate.jpg"), dilate)
-
-    open_morph = get_open_morph(image)
-    cv2.imwrite(os.path.join(save_dir, "open_morph.jpg"), open_morph)
-
-    close_morph = get_close_morph(image)
-    cv2.imwrite(os.path.join(save_dir, "close_morph.jpg"), close_morph)
-
-    gradient = get_gradient(image)
-    cv2.imwrite(os.path.join(save_dir, "gradient.jpg"), gradient)
-
     # 超特征搜索
-    # hy = []
-    # hy_num =
+    hy_params = [
+        {"name": ["scharrx64", "scharry64", "scharrxy64"], "func": "get_scharr"},
+        {"name": ["adaptive_threshold"], "func": "get_adaptive_threshold"},
+        {"name": ["threshold"], "func": "get_threshold"},
+        {"name": ["sobelx", "sobely", "sobelxy"], "func": "get_sobel"},
+        {"name": ["laplace"], "func": "get_laplace"},
+        {"name": ["blur"], "func": "get_blur"},
+        {"name": ["medianBlur"], "func": "get_medianBlur"},
+        {"name": ["GaussianBlur"], "func": "get_GaussianBlur"},
+        {"name": ["Canny"], "func": "get_Canny"},
+        {"name": ["profile"], "func": "graytoprofile"},
+        {"name": ["erode"], "func": "get_erode"},
+        {"name": ["dilate"], "func": "get_dilate"},
+        {"name": ["open_morph"], "func": "get_open_morph"},
+        {"name": ["close_morph"], "func": "get_close_morph"},
+        {"name": ["gradient"], "func": "get_gradient"},
+        {"name": ["tophat"], "func": "get_tophat"},
+        {"name": ["blackhat"], "func": "get_blackhat"},
+    ]
 
-    tophat = get_tophat(image)
-    cv2.imwrite(os.path.join(save_dir, "tophat.jpg"), tophat)
+    def hy_func(func=None, name_list=[], result=None, now_layers_num=1, layers_num=2):
+        """
 
-    blackhat = get_blackhat(image)
-    cv2.imwrite(os.path.join(save_dir, "blackhat.jpg"), blackhat)
+        Args:
+            func:
+            name_list:
+            result:
+            now_layers_num: 当前的层数
+            layers_num: 总层数
+
+        Returns:
+
+        """
+        assert layers_num < 3 # 最多找3层，因为计算量特大
+        for hy2 in hy_params:  # 第一层
+            names2 = hy2["name"]
+            func2 = hy2["func"]
+            if (func is None) or (func is not None and func != func2):
+
+                results2 = globals()[func2](result)
+                if len(names2) == 1:
+                    results2 = [results2]
+                for name2, result2 in zip(names2, results2):
+                    if len(name_list) == 0:
+                        filename = "{}.jpg".format(name2)
+                    else:
+                        filename = "{}_{}.jpg".format("_".join(name_list), name2)
+
+                    cv2.imwrite(os.path.join(save_dir, filename), result2)
+
+                    if layers_num > now_layers_num:
+                        name_list2 = copy.deepcopy(name_list)
+                        name_list2.append(name2)
+                        hy_func(func2, name_list2, result2, now_layers_num + 1, layers_num)  # 下一层
+
+    hy_func(result=image)
+
